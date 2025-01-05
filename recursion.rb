cd6a3fb0926c65cd0f88c188fd82e9b33d863c06
gabriele92984 <60701037+gabriele92984@users.noreply.github.com>
2025-01-05 15:27:22 +0100

    Fibonacci sequence using iteration

---
+++ recursion.rb
@@ -0,0 +1,12 @@
+def fibs(num)
+  fib_sequence = [0, 1] 
+
+  (2..num).each do |i|
+    fib_sequence << fib_sequence[i - 1] + fib_sequence[i - 2]
+  end
+
+  fib_sequence[0..num - 1] 
+end
+
+# Example usage
+puts fibs(8) # Output: [0, 1, 1, 2, 3, 5, 8, 13]
